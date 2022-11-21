2b969a4bc283754885d8dc476cac3c570b04f4cf
hami-r <mynameissaitama54@gmail.com>
2022-11-21 10:14:57 +0530

    Menu added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("<-----------MENU-------------->\n" +
+                "Select options:\n" +
+                "1. Addition\n" +
+                "2. Subtraction\n"+
+                "3. Multiplication\n" +
+                "4. Division\n" +
+                "To continure press 'y' and 'n' to exit"
+        );
     }
 }
